5cfcf1c39d94ded72cc09f89ee4f9265f47e008f
wendy sarrett <cepwin@gmail.com>
2017-05-30 22:13:57 -0400

    updated model

--- model4.R
+++ model4.R
@@ -2,9 +2,9 @@ require(Matrix)
 require(quanteda)
 require(readtext)
 require(data.table)
-require(ngram)
 require(tm)
 require(sqldf)
+require(RSQLite)
 
 ##need to create a structure that is n-1 gram, ngram #ngram/#n-1 gram
 
@@ -17,12 +17,12 @@ addPrev<-function(dt) {
 }
 
 
-doLoad<-function() {
+doLoad<-function(start,end) {
   text_blogs = readLines("./final/en_US/en_US.blogs.txt")
   text_news = readLines("./final/en_US/en_US.news.txt")
   text_twit = readLines("./final/en_US/en_US.twitter.txt")
   lenBlogs <-length(text_blogs)
-  groups<-(lenBlogs*0.35)/10000 + 1
+  groups<-(lenBlogs*0.4)/10000 + 1
   lStrBlogs<-""
   for(i in 1:groups) {
     st<- (i-1)*10000 + 1
@@ -32,7 +32,7 @@ doLoad<-function() {
   print("loaded blogs")
   
   lenNews <-length(text_news)
-  groups<-(lenNews*0.6)/10000 + 1
+  groups<-(lenNews*0.7)/10000 + 1
   lStrNews<-""
   for(i in 1:groups) {
     st<- (i-1)*10000 + 1
@@ -59,9 +59,9 @@ doLoad<-function() {
   newdf<-list()
   totLst<-list()
   
-  for(i in 2:6 ) {
+  for(i in start:end ) {
     gc()
-    ng<-quanteda::tokenize(text_string,ngrams=i,concatenator=" ", remove_punct=TRUE, remove_numbers=TRUE,remove_symbols=TRUE)
+    ng<-quanteda::tokenize(text_string,ngrams=i,concatenator=" ", remove_punct=TRUE, remove_twitter=TRUE,remove_numbers=TRUE,remove_symbols=TRUE)
     print("tokenized")
     myDfm<-dfm(ng)
     x<-colSums(myDfm)
@@ -85,7 +85,7 @@ doLoad<-function() {
 
 readDts<-function() {
   newdf<-list()
-  for(i in 1:5) {
+  for(i in 1:7) {
     new1<-as.data.table(fread(paste("dt",i,sep = "")))
     setkey(new1,V1)
     newdf[[i]]<-new1
@@ -93,10 +93,62 @@ readDts<-function() {
   newdf  
 }
 
-dofind<-function(phase,n,tables,discount) {
+putInSQL<-function() {
+  conn<-dbConnect(SQLite(),'ngramdb.db')
+  for(i in 1:9 ) {
+    new1<-as.data.table(fread(paste("dt",i,sep = "")))
+    dbWriteTable(conn, paste("dt",i,sep = ""), new1)
+  }
+  dbDisconnect(conn)
+}
+
+##this assumes newdf is in the environment
+rundofind<-function(phase) {
+  conn<-dbConnect(SQLite(),"ngramdb.db")
+  res<-dofind2(phase,1,conn)
+  print("got res")
+  fst<-res$V2
+  dbDisconnect(conn)
+ fstt<-quanteda::tokenize(fst[[1]],ngrams=1)
+ len<-length(fstt[[1]])
+ fstt[[1]][len]
+}
+
+dofind2<-function(phase,discount,conn) {
   ##for each phase entered
-  tableSearch<-tables[[n]]
   phase<-tolower(phase)
+  z<-quanteda::tokenize(phase,ngrams=1,remove_punct=TRUE, remove_twitter=TRUE,remove_numbers=TRUE,remove_symbols=TRUE)
+  numlen<-length(z[[1]])
+  print(numlen)
+
+  tableName<-paste("dt",numlen,sep="")
+  res<-dbGetQuery(conn,paste("select * from ", "dt4" ," where V1='",phase,"' order by V3 desc limit 1",sep=""))
+  if(nrow(res) == 1 && is.na(res$V3)) {
+    "phase not found"
+  }
+  if(nrow(res) == 1) {
+    res
+  } else {
+    ##  z<-strsplit(phase," ")
+    ##  numlen<-length(z[[1]])
+    if(numlen>1) {
+      phase<-concatenate(as.vector(z[[1]][2:numlen]))
+      res<-dofind(phase,discount*0.4,conn)
+    } else {
+      "phase not found"
+    }
+  }
+  
+}
+
+dofind<-function(phase,tables,discount) {
+  ##for each phase entered
+  phase<-tolower(phase)
+  z<-quanteda::tokenize(phase,ngrams=1,remove_punct=TRUE, remove_twitter=TRUE,remove_numbers=TRUE,remove_symbols=TRUE)
+  numlen<-length(z[[1]])
+  print(numlen)
+  tableSearch<-tables[[numlen]]
+  
   ## convert to lower case  (either replace " " to "_" or remove when creating phase table)
   ##searchterm<-concatenate(c(phase," [a-z]+$"),collapse="")
   tableSearch$V3<-as.numeric(tableSearch$V3)*discount
@@ -110,13 +162,13 @@ dofind<-function(phase,n,tables,discount) {
   if(nrow(res) > 1) {
     res[order(-rank(V3))]
   } else {
-    z<-strsplit(phase," ")
-    numlen<-length(z[[1]])
+  ##  z<-strsplit(phase," ")
+  ##  numlen<-length(z[[1]])
     if(numlen>1) {
       ##removing table from memory
       tablesearch<-"" 
       phase<-concatenate(as.vector(z[[1]][2:numlen]))
-      res<-dofind(phase,n-1,tables,discount*0.4)
+      res<-dofind(phase,tables,discount*0.4)
     } else {
       "phase not found"
     }
