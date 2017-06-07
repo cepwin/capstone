ff8e6191ed41271012f93a37e31b2c93ef81e7f2
wendy sarrett <cepwin@gmail.com>
2017-06-06 21:31:12 -0400

    fixed app

--- WordPredict/server.R
+++ WordPredict/server.R
@@ -7,20 +7,59 @@
 #    http://shiny.rstudio.com/
 #
 
+library(tm)
+library(ngram)
 library(shiny)
+library(quanteda)
+library(data.table)
+library(sqldf)
+library(RSQLite)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+  ##this assumes newdf is in the environment
+ getNextWord<-reactive ({
+    phase<-input$phrase
+    conn<-dbConnect(SQLite(),"ngramdb.db")
+    res<-dofind2(phase,1,conn)
+    fst<-res
+    dbDisconnect(conn)
+    fst
   })
+  
+  dofind2<-function(phase,discount,conn) {
+    ##for each phase entered
+    phase<-tolower(phase)
+    z<-quanteda::tokenize(phase,ngrams=1,remove_punct=TRUE, remove_twitter=TRUE,remove_numbers=TRUE,remove_symbols=TRUE)
+    numlen<-length(z[[1]])
+    print(paste("pre ",numlen))
+    if(numlen>5) {
+      phase<-concatenate(as.vector(z[[1]][(numlen-4):numlen]))
+      numlen<-5
+    }
+    print(numlen)
+    if(numlen > 0) {
+    tableName<-paste("dt",numlen,sep="")
+    res<-dbGetQuery(conn,paste("select v2 from ",tableName ," where v1 ='",phase,"' order by V3 desc limit 1",sep=""))
+   # if(nrow(res) == 1 && is.na(res)) {
+    #  "phase not found"
+    #}
+    if(nrow(res) == 1) {
+      res[[1]]
+    } else {
+      ##  z<-strsplit(phase," ")
+      ##  numlen<-length(z[[1]])
+      if(numlen>1) {
+        phase<-concatenate(as.vector(z[[1]][2:numlen]))
+        res<-dofind2(phase,discount*0.4,conn)
+      } else {
+        "phase not found"
+      }
+    }
+    }
+  }
+  
+  output$nextWord<-renderText({getNextWord()})
   
 })
--- WordPredict/ui.R
+++ WordPredict/ui.R
@@ -13,21 +13,18 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Text Prediction"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+       textInput("phrase","Enter Phase:"),
+       submitButton("Submit")
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       textOutput("nextWord")
     )
   )
 ))
